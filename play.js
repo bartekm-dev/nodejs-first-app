ea921b8b41672f52f89281dd337bbe4480b101ea
Bartek <bartekm.dev@gmail.com>
2022-10-27 10:27:41 +0100

    initial commit

---
+++ play.js
@@ -0,0 +1,15 @@
+let name = 'Michal';
+let age = 2;
+let isLoud = true;
+
+let user = {};
+
+function summarizeBaby(babyName, babyAge, babyIsLoud) {
+    return user = {
+        name: babyName,
+        age: babyAge,
+        loud: isLoud
+    }
+}
+
+console.log(summarizeBaby(name, age, isLoud))
