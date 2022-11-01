b146ac37fb1cfc6b9152d22a224afbcf0379f987
Bartek <bartekm.dev@gmail.com>
2022-11-01 09:26:25 +0000

    after installing express js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require('http');
+const express = require('express');
 
-//const routes = require('./routes')
+const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) => {
+    console.log('In the middlearth');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('Even further In the middlearth');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the Mordor now ');
+    res.send(`<h1>You are <b>Sauron's</b> bitch now</h1>`);
+});
+
+//const server = http.createServer(app);
+//server.listen(3000);
+// Changes to:
+app.listen(3000);
